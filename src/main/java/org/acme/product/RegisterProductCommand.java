9fb4bccff0dedef9b27a89db6881144f9dcf156b
Mateus Anjos <macedodosanjosmateu@gmail.com>
2025-07-09 17:37:09 -0300

    feat: refactor RegisterProductCommand to extend BaseCommand and create RegisterProductDto for data transfer

--- src/main/java/org/acme/product/RegisterProductCommand.java
+++ src/main/java/org/acme/product/RegisterProductCommand.java
@@ -1,56 +1,13 @@
 package org.acme.product;
 
-import java.util.List;
-import java.util.Map;
+import org.acme.core.edaes.BaseCommand;
 
-import org.acme.core.edaes.Command;
-import org.acme.core.edaes.Header;
+public class RegisterProductCommand extends BaseCommand<RegisterProductDto> {
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
-
-public class RegisterProductCommand implements Command {
-
-    @NotNull(message = "{sku.obrigatorio}")
-    @Pattern(regexp = "^[a-zA-Z0-9]{8}$", message = "{sku.pattern}")
-    String sku;
-
-    @NotNull(message = "{nome.obrigatorio}")
-    @Size(min = 3, max = 40, message = "{nome.size}")
-    @Pattern(regexp = "^[a-zA-Z0-9\\s-]+$", message = "{nome.pattern}")
-    String name;
-
-    @Pattern(regexp = "^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$", message = "{id.pattern}")
-    private String id;
-
-    private String type = RegisterProductCommand.class.getSimpleName();
-
-    public RegisterProductCommand(
-            String id,
-            String sku,
-            String name) {
-        this.id = id;
-        this.sku = sku;
-        this.name = name;
+    private RegisterProductCommand() {
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public Object getData() {
-        return Map.of(
-            "sku", sku,
-            "name", name
-        );
-    }
-
-    public List<Header> getHeaders() {
-        return List.of();
+    public static BaseCommand<RegisterProductDto> fromDto(RegisterProductDto dto) {
+        return new RegisterProductCommand().withData(dto);
     }
 }
---
+++ src/main/java/org/acme/product/RegisterProductDto.java
@@ -0,0 +1,17 @@
+package org.acme.product;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+
+public class RegisterProductDto {
+    @NotNull(message = "{sku.obrigatorio}")
+    @Pattern(regexp = "^[a-zA-Z0-9]{8}$", message = "{sku.pattern}")
+    public String sku;
+
+    @NotNull(message = "{nome.obrigatorio}")
+    @Size(min = 3, max = 40, message = "{nome.size}")
+    @Pattern(regexp = "^[a-zA-Z0-9\\s-]+$", message = "{nome.pattern}")
+    public String name;
+}
