f4b8a6db5837455df4b0b061847c3ec2b2be9f02
Mateus Anjos <macedodosanjosmateu@gmail.com>
2025-07-09 17:44:49 -0300

    feat: extend Command interface to implement Serializable for improved data handling

--- src/main/java/org/acme/core/edaes/Command.java
+++ src/main/java/org/acme/core/edaes/Command.java
@@ -1,8 +1,9 @@
 package org.acme.core.edaes;
 
+import java.io.Serializable;
 import java.util.List;
 
-public interface Command<T> {
+public interface Command<T> extends Serializable {
     T getData();
     List<Header> getHeaders();
 }
