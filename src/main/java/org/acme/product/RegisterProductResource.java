82a97ecdbf9b018e67c30b9e74bdf7d5502a76dd
Mateus Anjos <macedodosanjosmateu@gmail.com>
2025-07-09 17:38:51 -0300

    feat: integrate HeaderService for generating default headers in product registration

--- src/main/java/org/acme/product/RegisterProductResource.java
+++ src/main/java/org/acme/product/RegisterProductResource.java
@@ -1,6 +1,10 @@
 package org.acme.product;
 
+import org.acme.core.edaes.Command;
+import org.acme.core.filter.HeaderService;
+
 import io.smallrye.mutiny.Uni;
+import jakarta.inject.Inject;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
@@ -14,8 +18,14 @@ import jakarta.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 public class RegisterProductResource {
 
+    @Inject
+    HeaderService headerService;
+
     @POST
-    public Uni<Response> registerProduct(@Valid RegisterProductCommand command) {
-        return Uni.createFrom().item(Response.created(null).entity(command).build());
+    public Uni<Response> registerProduct(@Valid RegisterProductDto dto) {
+        Command<RegisterProductDto> command = RegisterProductCommand.fromDto(dto)
+        .withHeaders(headerService.generateDefaultHeaders());
+
+        return Uni.createFrom().item(Response.created(null).entity(command.getData()).build());
     }
 }
