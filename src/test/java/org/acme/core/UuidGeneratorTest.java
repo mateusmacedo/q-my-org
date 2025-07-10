81a67ae01f9514b680587991d8bb2dab03be7bc7
Mateus Anjos <macedodosanjosmateu@gmail.com>
2025-07-10 01:28:14 -0300

    refactor: remove unused import for UuidGenerator in UuidGeneratorTest

--- src/test/java/org/acme/core/UuidGeneratorTest.java
+++ src/test/java/org/acme/core/UuidGeneratorTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.acme.core.edaes.UuidGenerator;
-
 @QuarkusTest
 public class UuidGeneratorTest {
     @Test
